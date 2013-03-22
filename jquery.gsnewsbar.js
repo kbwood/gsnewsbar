0e51879946be530639201584bea9ae75082cf681
Keith Wood <kbwood@iinet.net.au>
2013-03-22 16:11:17 +1100

    Release 1.1.0

--- jquery.gsnewsbar.js
+++ jquery.gsnewsbar.js
@@ -1,9 +1,8 @@
 /* http://keith-wood.name/gsnewsbar.html
-   Google Search Newsbar for jQuery v1.0.2.
+   Google Search Newsbar for jQuery v1.1.0.
    See http://www.google.com/uds/solutions/newsbar/reference.html.
    Written by Keith Wood (kbwood{at}iinet.com.au) November 2008.
-   Dual licensed under the GPL (http://dev.jquery.com/browser/trunk/jquery/GPL-LICENSE.txt) and 
-   MIT (http://dev.jquery.com/browser/trunk/jquery/MIT-LICENSE.txt) licenses. 
+   Available under the MIT (https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt) license. 
    Please attribute the author if you use it. */
 
 /* Display a Google Search Newsbar.
@@ -29,11 +28,11 @@ function GSNewsbar() {
 	};
 }
 
-var PROP_NAME = 'gsnewsbar';
-
 $.extend(GSNewsbar.prototype, {
 	/* Class name added to elements to indicate already configured with GSNewsbar. */
 	markerClassName: 'hasGSNewsbar',
+	/* Name of the data property for instance settings. */
+	propertyName: 'gsnewsbar',
 
 	/* Cycle times. */
 	cycleVShort: 3000,
@@ -51,124 +50,131 @@ $.extend(GSNewsbar.prototype, {
 	targetParent: '_parent',
 
 	/* Override the default settings for all GSNewsbar instances.
-	   @param  options  (object) the new settings to use as defaults */
+	   @param  options  (object) the new settings to use as defaults
+	   @return  (GSNewsbar) this object */
 	setDefaults: function(options) {
-		extendRemove(this._defaults, options || {});
+		$.extend(this._defaults, options || {});
 		return this;
 	},
 
 	/* Attach the newsbar widget to a div.
-	   @param  target   (element) the affected division
-	   @param  options  (object) the new instance settings */
-	_attachGSNewsbar: function(target, options) {
+	   @param  target   (element) the control to affect
+	   @param  options  (object) the custom options for this instance */
+	_attachPlugin: function(target, options) {
 		target = $(target);
-		if (target.is('.' + this.markerClassName)) {
+		if (target.hasClass(this.markerClassName)) {
 			return;
 		}
-		target.addClass(this.markerClassName);
-		var inst = {target: target};
-		inst.options = $.extend({}, options);
-		$.data(target[0], PROP_NAME, inst);
-		this._updateGSNewsbar(target, inst);
+		var inst = {options: $.extend({}, this._defaults, options), target: target};
+		target.addClass(this.markerClassName).data(this.propertyName, inst);
+		this._optionPlugin(target, options);
 	},
 
-	/* Reconfigure the settings for a newsbar div.
-	   @param  target   (element) the affected division
-	   @param  options  (object) the new/changed instance settings */
-	_changeGSNewsbar: function(target, options) {
-		var inst = $.data(target, PROP_NAME);
-		if (inst) {
-			extendRemove(inst.options, options || {});
-			$.data(target, PROP_NAME, inst);
-			this._updateGSNewsbar($(target), inst);
+	/* Retrieve or reconfigure the settings for a control.
+	   @param  target   (element) the control to affect
+	   @param  options  (object) the new options for this instance or
+	                    (string) an individual property name
+	   @param  value    (any) the individual property value (omit if options
+	                    is an object or to retrieve the value of a setting)
+	   @return  (any) if retrieving a value */
+	_optionPlugin: function(target, options, value) {
+		target = $(target);
+		var inst = target.data(this.propertyName);
+		if (!options || (typeof options == 'string' && value == null)) { // Get option
+			var name = options;
+			options = (inst || {}).options;
+			return (options && name ? options[name] : options);
 		}
+
+		if (!target.hasClass(this.markerClassName)) {
+			return;
+		}
+		options = options || {};
+		if (typeof options == 'string') {
+			var name = options;
+			options = {};
+			options[name] = value;
+		}
+		$.extend(inst.options, options);
+		this._updateGSNewsbar(target[0], inst);
+	},
+
+	/* Redisplay the newsbar with an updated display.
+	   @param  target  (element) the affected division
+	   @param  inst    (object) the instance settings */
+	_updateGSNewsbar: function(target, inst) {
+		var getElement = function(selector) {
+			var element = inst.options[selector];
+			element = (element ? (element.jQuery ? element : $(element)) : null);
+			return (element && element.length ? element[0] : null);
+		};
+		var search = inst.options.search;
+		search = ($.isArray(search) ? search : [search]);
+		inst.newsbar = new GSnewsBar(target,
+			{largeResultSet: inst.options.manyResults, horizontal: inst.options.horizontal,
+			resultStyle: (inst.options.verticalCompressed ? 2 : 1),
+			title: inst.options.title, linkTarget: inst.options.linkTarget,
+			currentResult: getElement('currentResult'),
+			autoExecuteList: {executeList: search, cycleTime: inst.options.cycleTime,
+				cycleMode: inst.options.cycleMode}});
 	},
 
 	/* Perform a new seacrh in the newsbar.
 	   @param  target  (element) the affected division
 	   @param  search  (string) the new search terms */
-	_searchGSNewsbar: function(target, search) {
-		var inst = $.data(target, PROP_NAME);
+	_searchPlugin: function(target, search) {
+		var inst = $.data(target, this.propertyName);
 		if (inst) {
-			extendRemove(inst.options, {search: search});
-			$.data(target, PROP_NAME, inst);
+			$.extend(inst.options, {search: search});
 			inst.newsbar.execute(search);
 		}
 	},
 
-	/* Redisplay the newsbar with an updated display.
-	   @param  target  (object) the jQuery object for the affected division
-	   @param  inst    (object) the instance settings */
-	_updateGSNewsbar: function(target, inst) {
-		var getElement = function(selector) {
-			var element = $.gsnewsbar._get(inst, selector);
-			element = (element ? (element.jQuery ? element : $(element)) : null);
-			return (element && element.length ? element[0] : null);
-		};
-		var search = this._get(inst, 'search');
-		search = (isArray(search) ? search : [search]);
-		inst.newsbar = new GSnewsBar(target[0],
-			{largeResultSet: this._get(inst, 'manyResults'),
-			horizontal: this._get(inst, 'horizontal'),
-			resultStyle: (this._get(inst, 'verticalCompressed') ? 2 : 1),
-			title: this._get(inst, 'title'),
-			linkTarget: this._get(inst, 'linkTarget'),
-			currentResult: getElement('currentResult'),
-			autoExecuteList: {executeList: search,
-				cycleTime: this._get(inst, 'cycleTime'),
-				cycleMode: this._get(inst, 'cycleMode')}});
-	},
-
-	/* Remove the newsbar widget from a div.
-	   @param  target  (element) the affected division */
-	_destroyGSNewsbar: function(target) {
+	/* Remove the plugin functionality from a control.
+	   @param  target  (element) the control to affect */
+	_destroyPlugin: function(target) {
 		target = $(target);
-		if (!target.is('.' + this.markerClassName)) {
+		if (!target.hasClass(this.markerClassName)) {
 			return;
 		}
-		target.removeClass(this.markerClassName).empty();
-		$.removeData(target[0], PROP_NAME);
-	},
-
-	/* Get a setting value, defaulting if necessary.
-	   @param  inst  (object) the instance settings
-	   @param  name  (string) the name of the setting
-	   @return  (any) the setting value */
-	_get: function(inst, name) {
-		return (inst.options[name] != null ?
-			inst.options[name] : $.gsnewsbar._defaults[name]);
+		target.removeClass(this.markerClassName).empty().removeData(this.propertyName);
 	}
 });
 
-/* jQuery extend now ignores nulls! */
-function extendRemove(target, props) {
-	$.extend(target, props);
-	for (var name in props) {
-		if (props[name] == null) {
-			target[name] = null;
-		}
-	}
-	return target;
-}
+// The list of commands that return values and don't permit chaining
+var getters = [];
 
-/* Determine whether an object is an array. */
-function isArray(a) {
-	return (a && a.constructor == Array);
+/* Determine whether a command is a getter and doesn't permit chaining.
+   @param  command    (string, optional) the command to run
+   @param  otherArgs  ([], optional) any other arguments for the command
+   @return  true if the command is a getter, false if not */
+function isNotChained(command, otherArgs) {
+	if (command == 'option' && (otherArgs.length == 0 ||
+			(otherArgs.length == 1 && typeof otherArgs[0] == 'string'))) {
+		return true;
+	}
+	return $.inArray(command, getters) > -1;
 }
 
 /* Attach the GSNewsbar functionality to a jQuery selection.
-   @param  command  (string) the command to run (optional, default 'attach')
-   @param  options  (object) the new settings to use for these GSNewsbar instances
-   @return  (object) jQuery object for chaining further calls */
+   @param  options  (object) the new settings to use for these instances (optional) or
+                    (string) the command to run (optional)
+   @return  (jQuery) for chaining further calls or
+            (any) getter value */
 $.fn.gsnewsbar = function(options) {
 	var otherArgs = Array.prototype.slice.call(arguments, 1);
+	if (isNotChained(options, otherArgs)) {
+		return plugin['_' + options + 'Plugin'].apply(plugin, [this[0]].concat(otherArgs));
+	}
 	return this.each(function() {
 		if (typeof options == 'string') {
-			$.gsnewsbar['_' + options + 'GSNewsbar'].
-				apply($.gsnewsbar, [this].concat(otherArgs));
+			if (!plugin['_' + options + 'Plugin']) {
+				throw 'Unknown command: ' + options;
+			}
+			plugin['_' + options + 'Plugin'].apply(plugin, [this].concat(otherArgs));
 		}
 		else {
-			$.gsnewsbar._attachGSNewsbar(this, options);
+			plugin._attachPlugin(this, options || {});
 		}
 	});
 };
@@ -188,6 +194,6 @@ document.write('<script type="text/javascript" src="http://www.google.com/uds/' 
 	'rel="stylesheet"/>\n');
       
 /* Initialise the GSNewsbar functionality. */
-$.gsnewsbar = new GSNewsbar(); // singleton instance
+var plugin = $.gsnewsbar = new GSNewsbar(); // Singleton instance
 
 })(jQuery);
